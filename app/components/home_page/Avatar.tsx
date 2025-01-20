632b7cca1f6982b086d04ebc7285b16b93353a8c
me <slavaveto@gmail.com>
2025-01-20 16:52:18 -0300

    fаватар

--- app/components/home_page/Avatar.tsx
+++ app/components/home_page/Avatar.tsx
@@ -1,6 +1,8 @@
 "use client";
 import React from "react";
 import LocalText from "@/app/assets/localText";
+import Image from 'next/image';
+
 
 export default function Avatar() {
 
@@ -9,15 +11,22 @@ export default function Avatar() {
             <div
                 className="flex flex-row items-center justify-between -pr-[20px]  -xs390:gap-10"
             >
-                <img
+                <Image
                     src="/me.jpg"
                     alt="Описание изображения"
+                    // layout="responsive"
+                    width={120} // Пропорции, не конкретная ширина
+                    height={120} // Пропорции, не конкретная высота
+                    priority // Флаг для прелоадинга
                     className="block dark:hidden w-[120px] rounded-full  border-3 border-white"
                     style={{boxShadow: "0 0px 10px rgba(0, 0, 0, 0.2)"}}
                 />
-                <img
+                <Image
                     src="/me_dark.jpg"
                     alt="Описание изображения"
+                    width={120} // Пропорции, не конкретная ширина
+                    height={120} // Пропорции, не конкретная высота
+                    priority // Флаг для прелоадинга
                     className="hidden dark:block w-[120px] rounded-full  border-0"
                     style={{boxShadow: "0 0px 10px rgba(0, 0, 0, 0.2)"}}
                 />
