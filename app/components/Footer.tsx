e1afd2494708bc9bbdac1e03a727d56a74e206c5
me <slavaveto@gmail.com>
2025-01-26 11:08:38 -0300

    -

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -9,7 +9,7 @@ export default function Footer ({ width }: { width: string }) {
     //console.log(width);
 
     return (
-        <footer className="footer_bg flex --h-[65px] pt-[10px] pb-[15px] md:pt-[15px] md:pb-[18px] mt-[35px] items-center ">
+        <footer className="footer_bg flex --h-[65px] pt-[10px] pb-[15px] md:pt-[15px] md:pb-[18px] mt-[50px] items-center ">
             <div className="container flex mx-auto px-3 -justify-between justify-end items-center"
                  style={{ maxWidth: `${width}px`}}>
                 {/*<div>*/}
